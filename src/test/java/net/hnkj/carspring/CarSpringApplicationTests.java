2cd968a060a46e5bb69fa68709eae5e1ac382937
alone <2937957762@qq.com>
2022-12-09 18:47:58 +0800

    Initial commit

---
+++ src/test/java/net/hnkj/carspring/CarSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package net.hnkj.carspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
